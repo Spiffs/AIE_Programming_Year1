98a987b03ebc6a01d8dd4aefc5a49cd87b4603b2
Oscar Smith <s192134@students.aie.edu.au>
2020-09-10 16:55:35 +1000

    CrossPlatform 03

--- CrossPlatformDevelopment/Oscar_Smith_CrossPlatform_FullTron/Assets/Scripts/PlayerMovement.cs
+++ CrossPlatformDevelopment/Oscar_Smith_CrossPlatform_FullTron/Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float velocity = 5;
-    public float turnSpeed = 10;
+    public Rigidbody rb;
 
-    Vector2 input;
-    float angle;
-
-    Quaternion targetRotation;
-    Transform cam;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {   
-
-    }
-
+    public float moveSpeed = 1f;
+    public float turnSpeed = 2f;
     // Update is called once per frame
     void Update()
     {
+        // AddForce FORWARD
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         
+        Vector3 force = moveSpeed * vertical * transform.forward;
+
+        if (rb.velocity.magnitude > 15)
+        {
+            rb.velocity = rb.velocity.normalized * 15;
+        }
+
+        rb.AddForce(force, ForceMode.Acceleration);
+
+        if (rb.velocity.magnitude > 1)
+        {
+            rb.AddTorque(transform.up * turnSpeed * horizontal * (25 - rb.velocity.magnitude));
+        }
     }
 }
